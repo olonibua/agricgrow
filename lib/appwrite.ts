612868e4f07a923abfe250a1ff098c4d1a4ebaf3
olonibua tolulope <olonibua0001@gmail.com>
2025-03-05 16:24:01 +0000

    new

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -11,7 +11,7 @@ export const databases = new Databases(client);
 export const storage = new Storage(client);
 
 // Export ID for convenience
-export { ID };
+export { ID, Query };
 
 // Database and collection IDs
 export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || '';
